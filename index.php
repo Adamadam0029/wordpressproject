ee9bc21b45b208700a8b7b01257d024cda9d1255
ekrem-nl <9028034@student.zadkine.nl>
2026-06-22 13:11:34 +0200

    bijwerking

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-
 <?php get_header(); ?>
-
-<main>
-
-
-<h2>Dit is een tesrt</h2>
-<div class="buttons">
-
-        <a href="<?php  echo get_field("url-cta-1"); ?>" class="wp-block-button__link wp-element-button"><?php  echo get_field("url-cta-1"); ?></a>
-        <a href="<?php  echo get_field("url-cta-1"); ?>" class="wp-block-button__link wp-element-button"><?php  echo get_field("url-cta-1"); ?></a>
-
-
-
-</div>
 
 <?php
 while (have_posts()) :
